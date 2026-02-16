9d347e0143d6e08cb0d4faeb73f081af86d08110
2400031265@kluniversity.in <2400031265@kluniversity.in>
2026-02-16 15:18:46 +0530

    StudentMVCApp

---
+++ StudentMVCApp/src/main/java/com/klu/servic/StudentService.java
@@ -0,0 +1,14 @@
+package com.klu.servic;
+import java.util.List;
+import com.klu.model.Student;
+public interface StudentService {
+
+String getWelcomeMessage();
+	Student CreateStudent(Student student);
+	Student getStudentById(int id);
+	List<Student> getAllStudents();
+	Student updateStudent(int id, Student student);
+	String deleteStudent(int id);
+	List<Student> searchStudent(String name, String course);
+
+}
